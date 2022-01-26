8881a0d7673b23af770b1a13c93aeef7f14cca7c
jole <jonas@jole.xyz>
2022-01-26 17:21:05 +0100

    clean up code (still stable output)

--- jd.c
+++ jd.c
@@ -5,25 +5,11 @@
 #include <stdlib.h>
 #include <math.h>
 
-/*╔════════╤═════════════════════════════════════════════════════════╗*/
-/*║ Bytes  │ Description                                             ║*/
-/*╠════════╪═════════════════════════════════════════════════════════╣*/
-/*║ 8      │ "farbfeld" magic value                                  ║*/
-/*╟────────┼─────────────────────────────────────────────────────────╢*/
-/*║ 4      │ 32-Bit BE unsigned integer (width)                      ║*/
-/*╟────────┼─────────────────────────────────────────────────────────╢*/
-/*║ 4      │ 32-Bit BE unsigned integer (height)                     ║*/
-/*╟────────┼─────────────────────────────────────────────────────────╢*/
-/*║ [2222] │ 4x16-Bit BE unsigned integers [RGBA] / pixel, row-major ║*/
-/*╚════════╧═════════════════════════════════════════════════════════╝*/
-
 #define magic "farbfeld"
 
-struct pixel {
-	int32_t r, g, b;
-};
+typedef int32_t pixel[3];
 
-struct pixel pal[128] = {
+pixel pal[128] = {
 	{0, 0, 0},
 	{UINT16_MAX, UINT16_MAX, UINT16_MAX},
 	{UINT16_MAX, 0, 0},
@@ -32,54 +18,58 @@ struct pixel pal[128] = {
 };
 size_t pal_len;
 
+float
+red_mean(pixel a, pixel b) {
+	float mean_r =    (a[0] + b[0])/2,
+		 d_r = abs(a[0] - b[0]),
+		 d_g = abs(a[1] - b[1]),
+		 d_b = abs(a[2] - b[2]);
+
+	float c_1 = (512+mean_r/(float)UINT16_MAX)*d_r*d_r;
+	float c_2 = 1024*d_g*d_g;
+	float c_3 = (512+ (UINT16_MAX-1-mean_r)/(float)UINT16_MAX)*d_b*d_b;
+
+	float c = sqrt(c_1 + c_2 + c_3);
+	return c;
+}
+
 static
 void
-closest_color(struct pixel in, struct pixel *out){
-	size_t bi = 0;
-	int32_t bd = INT32_MAX;
-	for(size_t i = 0; i < pal_len; ++i){
-		float rmean = (in.r + pal[i].r)/2,
-			dr = abs(in.r - pal[i].r),
-			dg = abs(in.g - pal[i].g),
-			db = abs(in.b - pal[i].b);
-
-		float d = abs(in.r - pal[i].r);
-			+ abs(in.g - pal[i].g);
-			+ abs(in.b - pal[i].b);
-
-		float c = sqrt((512+rmean/(float)UINT16_MAX)*dr*dr + 1024*dg*dg + (512+ (UINT16_MAX-1-rmean)/(float)UINT16_MAX)*db*db);
-		if(c < bd){
-			bd = c;
-			bi = i;
+closest_color(pixel in, pixel *out){
+	size_t  best_index = 0;
+	int32_t best_delta = INT32_MAX;
+	for(size_t i = 0; i < pal_len; ++i) {
+		float delta = red_mean(in, pal[i]);
+		if(delta < best_delta) {
+			best_delta = delta;
+			best_index = i;
 		}
 	}
-	memcpy(out, &pal[bi], sizeof(*out));
+	memcpy(out, &pal[best_index], sizeof(*out));
 }
 
 void
 set_pal() {
 	FILE *fp = fopen("pal", "r");
 	if (fp != NULL) {
-		size_t i = 0;
-		int32_t R, G, B;
-		while(fscanf(fp, "%2x%2x%2x", &R, &G, &B) == 3) {
-			pal[i].r = R * 256;
-			pal[i].g = G * 256;
-			pal[i].b = B * 256;
-			++i;
-			fprintf(stderr, "%d %d %d\n", R, G, B);
+		size_t color_i = 0;
+		int32_t input[3];
+		while(fscanf(fp, "%2x%2x%2x", &input[0], &input[1], &input[2]) == 3) {
+			for(size_t i = 0; i < 3; ++i)
+				pal[color_i][i] = input[i] * 256;
+			++color_i;
 		}
-		pal_len = i;
+		pal_len = color_i;
 		return;
 	}
 
 	// get length
 	size_t i = 0;
-	struct pixel now, next;
+	pixel now, next;
 	while(1){
-		now = pal[i];
-		next = pal[i+1];
-		if(now.r + now.g + now.b == 0 && next.r + next.g + next.b == 0) {
+		memcpy(&now,  pal[i],   sizeof(pixel));
+		memcpy(&next, pal[i+1], sizeof(pixel));
+		if(now[0] + now[1] + now[2] == 0 && next[0] + next[1] + next[2] == 0) {
 			pal_len = i;
 			return;
 		}
@@ -97,91 +87,101 @@ main(int argc, char* argv[]){
 	// set to 2 for B/W
 	set_pal();
 
-	uint8_t header[8];
+	size_t len_magic = strlen(magic);
+	uint8_t header[len_magic];
 	fread(header, sizeof(*header), sizeof(header)/sizeof(*header), stdin);
-	if(memcmp(magic, header, 8)){
+	if(memcmp(magic, header, len_magic)){
 		fputs("wrong header\n", stderr);
 	}
 
 	uint32_t in[2];
 	fread(in, sizeof(*in), sizeof(in)/sizeof(*in), stdin);
-	uint32_t w = ntohl(in[0])
-		,h = ntohl(in[1]);
+	uint32_t w = ntohl(in[0]),
+		 h = ntohl(in[1]);
 
-	uint16_t *m = malloc(sizeof(*m)*w*h*4);
-	int32_t *o = malloc(sizeof(*o)*w*h*3);
+	// read src img into memory
+	uint16_t *out = malloc(sizeof(*out)*w*h*4); // finished image
+	int32_t  *tmp = malloc(sizeof(*tmp)*w*h*3); // work-in-progress image
 	for(size_t y = 0; y < h; ++y){
 		for(size_t x = 0; x < w; ++x){
+			// read next pixel
 			uint16_t v[4];
 			fread(v, sizeof(*v), sizeof(v)/sizeof(*v), stdin);
-			size_t i = y*w*4+x*4;
 
-			v[0] = ntohs(v[0]);
-			v[1] = ntohs(v[1]);
-			v[2] = ntohs(v[2]);
-			v[3] = ntohs(v[3]);
-			m[i+3] = v[3];
+			size_t idx_out = 4 * (y * w + x);
 
-			size_t i2 = y*w*3+x*3;
+			// fix endianness
+			for(size_t i = 0; i < 4; ++i)
+				v[i] = ntohs(v[i]);
 
-			o[i2] = v[0];
-			o[i2+1] = v[1];
-			o[i2+2] = v[2];
+			// copy alpha directly
+			out[idx_out+3] = v[3];
+
+			// tmp has only 3 channels
+			size_t idx_tmp = 3 * (y * w + x);
+
+			// copy src to tmp
+			for(size_t i = 0; i < 3; ++i)
+				tmp[idx_tmp+i]   = v[i];
 		}
 	}
 
 
+	// generate dithered image
 	for(size_t y = 0; y < h; ++y){
 		for(size_t x = 0; x < w; ++x){
-			size_t i = y*w*4+x*4;
+			size_t i_out = 4*(y*w+x);
+			size_t i_r = 3*(y*w+x);
+			size_t i_g = i_r + 1;
+			size_t i_b = i_r + 2;
 
-			size_t i0 = y*w*3+x*3;
-			size_t i1 = y*w*3+x*3+1;
-			size_t i2 = y*w*3+x*3+2;
-
-			struct pixel p_in = {o[i0], o[i1], o[i2]}, p_out;
+			// find output color
+			pixel p_in = {tmp[i_r], tmp[i_g], tmp[i_b]};
+			pixel p_out;
 			closest_color(p_in, &p_out);
 
-			struct pixel d = {
-				p_in.r - p_out.r,
-				p_in.g - p_out.g,
-				p_in.b - p_out.b
-			};
+			// copy found color to output
+			for(size_t o = 0; o < 3; ++o)
+				out[i_out+o] = p_out[0+o];
 
-			if(x < w-1){
-				o[i0+3] += 7*d.r / 16;
-				o[i1+3] += 7*d.g / 16;
-				o[i2+3] += 7*d.b / 16;
-			}
-			if(y < h-1){
-				if(x < w-1){
-					o[i0+3*w+3] += d.r / 16;
-					o[i1+3*w+3] += d.g / 16;
-					o[i2+3*w+3] += d.b / 16;
-				}
-				if(x > 0){
-					o[i0+3*w-3] += 3*d.r / 16;
-					o[i1+3*w-3] += 3*d.g / 16;
-					o[i2+3*w-3] += 3*d.b / 16;
-				}
-				o[i0+3*w] += 5*d.r / 16;
-				o[i1+3*w] += 5*d.g / 16;
-				o[i2+3*w] += 5*d.b / 16;
-			}
+			// find difference
+			pixel d;
+			for(size_t i = 0; i < 3; ++i)
+				d[i] = p_in[i] - p_out[i];
 
-			m[i] = p_out.r*1;
-			m[i+1] = p_out.g*1;
-			m[i+2] = p_out.b*1;
+			// correct future pixels
+			if(x < w-1) {
+				size_t offset = i_r+3;
+				for(size_t i = 0; i < 3; ++i)
+					tmp[offset + i] += 7*d[i] / 16;
+			}
+			if(y < h-1) {
+				if(x < w-1) {
+					size_t offset = i_r+3 * (w+1);
+					for(size_t i = 0; i < 3; ++i)
+						tmp[offset + i] += d[i] / 16;
+				}
+
+				if(x > 0) {
+					size_t offset = i_r+3 * (w-1);
+					for(size_t i = 0; i < 3; ++i)
+						tmp[offset + i] += 3*d[i] / 16;
+				}
+
+				size_t offset = i_r+3 * w;
+				for(size_t i = 0; i < 3; ++i)
+					tmp[offset + i] += 5*d[i] / 16;
+			}
 		}
 	}
 
 
 	for(size_t i = 0; i < h*w*4; ++i)
-		m[i] = htons(m[i]);
+		out[i] = htons(out[i]);
 
 	fwrite(header, sizeof(*header), sizeof(header)/sizeof(*header), stdout);
 	fwrite(in, sizeof(*in), sizeof(in)/sizeof(*in), stdout);
-	fwrite(m, sizeof(*m), sizeof(*m)*w*h*4, stdout);
+	fwrite(out, sizeof(*out), sizeof(*out)*w*h*4, stdout);
 
 	return 0;
 }
